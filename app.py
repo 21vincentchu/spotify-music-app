eafc5d708a01b7c2b9a24539093354a5196f4b84
Vincent Chu <21vincentchu@gmail.com>
2025-09-16 09:46:42 -0500

    changed port from 5000 to 8000 for macusers

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ if __name__ == '__main__':
 
     print(f"{song['name']} by {song['artists'][0]['name']}")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
