ccc2084d38914510642a8e86e923a268a5872e82
amandanngo <amandanngo@gmail.com>
2025-10-12 16:36:28 -0500

    Created base pages and connected them to Desktop Layout

--- frontend/reverb-client/src/layouts/DesktopLayout.js
+++ frontend/reverb-client/src/layouts/DesktopLayout.js
@@ -2,6 +2,12 @@
 import { Routes, Route } from "react-router-dom";
 
 import SignInPage from '../pages/shared/SignInPage';
+import NavbarDesktop from "../components/desktop/NavbarDesktop";
+import HomeDesktop from "../pages/desktop/HomeDesktop";
+import RatingsPage from "../pages/shared/RatingsPage";
+import StatisticsPage from "../pages/shared/StatisticsPage";
+import RecommendationsPage from "../pages/shared/RecommendationsPage";
+import FriendsPage from "../pages/shared/FriendsPage";
 
 
 function DesktopLayout(){
@@ -9,8 +15,20 @@ function DesktopLayout(){
     return(
         <div>
             <h1>Desktop Layout</h1>
+            <NavbarDesktop  />
             <Routes>
                 <Route path="/" element={<SignInPage />} />
+                <Route path="/home" element={<HomeDesktop />} />
+                <Route path="/ratings" element={<RatingsPage />} />
+                <Route path="/statistics" element={< StatisticsPage/>} />
+                <Route path="/recommendations" element={<RecommendationsPage />} />
+                <Route path="/friends" element={<FriendsPage />} />
+                
+                
+
+
+
+
             </Routes>
         </div>
     )
