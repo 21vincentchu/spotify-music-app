934b11f3be183122ef6b07827e21f9b65483068f
Vincent Chu <21vincentchu@gmail.com>
2025-10-16 11:43:26 -0500

    updated frontend folder

--- frontend/reverb-client/src/pages/desktop/HomeDesktop.js
+++ frontend/reverb-client/src/pages/desktop/HomeDesktop.js
@@ -2,20 +2,23 @@ import SongComponent from "../../components/shared/SongComponent";
 
 function HomeDesktop() {
   return (
-    <div>
-        <div className="home-recommended">
+    <div className="home-page">
+        <div className="home-recommended round-outline">
             <h2>Recommended</h2>
-            <SongComponent />
-            <SongComponent />
-            <SongComponent />
-            <SongComponent />
-            <SongComponent />
-            <SongComponent />
-            <SongComponent />
-            <SongComponent />
+            <div className="home-recommended-songs">
+                <SongComponent />
+                <SongComponent />
+                <SongComponent />
+                <SongComponent />
+                <SongComponent />
+                <SongComponent />
+                <SongComponent />
+                <SongComponent />
+            </div>
+        
 
         </div>
-        <div>
+        <div className="home-stats round-outline">
             <h2>Statistics</h2>
 
         </div>
