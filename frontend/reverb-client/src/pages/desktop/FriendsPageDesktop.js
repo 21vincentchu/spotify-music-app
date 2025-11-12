ae9df19572223734b421947f77d862caf6200fed
amandanngo <amandanngo@gmail.com>
2025-11-11 21:16:40 -0600

    Added fetch friends call

--- frontend/reverb-client/src/pages/desktop/FriendsPageDesktop.js
+++ frontend/reverb-client/src/pages/desktop/FriendsPageDesktop.js
@@ -1,6 +1,38 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import FriendComponentDesktop from "../../components/desktop/FriendComponentDesktop";
 
 function FriendsPage() {
+
+  const [isLoadingData, setIsLoadingRecentData] = useState(true);
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {   
+    getFriendsList();
+}, []);
+
+
+  const getFriendsList = async () => {
+    setIsLoadingRecentData(true);
+    axios.get('http://localhost:8000/api/friends', {
+        withCredentials: true
+      })
+      .then((response) => {
+        console.log('Friends:', response.data);
+        setFriends(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching recent data:', error);
+      })
+      .finally(() => {
+        setIsLoadingRecentData(false);
+      });
+}
+
+
+
+
   return (
     <div className="page">
       <div className="data-container round-outline blue-box-shadow">
