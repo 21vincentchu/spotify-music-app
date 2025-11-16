1c402fd97dcbe4afd52bc56c43e90a7ac48535fb
julieto1 <123041227+julieto1@users.noreply.github.com>
2025-11-16 17:13:51 -0600

    Editing Mobile Layouts

--- frontend/reverb-client/src/pages/mobile/HomeMobile.js
+++ frontend/reverb-client/src/pages/mobile/HomeMobile.js
@@ -11,7 +11,7 @@ const HomeMobile = () => {
         {/* ===== Featured Recommendations ===== */}
         <div className="scroll-section">
             <section className="home-featured">
-                      <h2>Featured Stats</h2>
+                      <h2>Top Songs</h2>
                       <p className="subtext">Songs You've Been Loving</p>
 
                       <div className="grid-wrapper">
@@ -92,8 +92,85 @@ const HomeMobile = () => {
         
         <div className="scroll-section">
             <section className="home-featured">
-                      <h2>Featured Reccomendations</h2>
+                      <h2>Featured Recomendations</h2>
                       <p className="subtext">Your friends want you to listen to...</p>
+
+                      <div className="grid-wrapper">
+                        <div className="responsive-grid">
+                          <div className="song-info-box">
+                            <div className="circle"></div>
+                            <div className="song-text">
+                              <p className="song-title">Lost in Japan</p>
+                              <p className="song-artist">Shawn Mendes</p>
+                              <p className="song-genre">Pop</p>
+                            </div>
+                          </div>
+
+                          <div className="song-info-box">
+                            <div className="circle"></div>
+                            <div className="song-text">
+                              <p className="song-title">Levitating</p>
+                              <p className="song-artist">Dua Lipa</p>
+                              <p className="song-genre">Dance</p>
+                            </div>
+                          </div>
+                          <div className="song-info-box">
+                            <div className="circle"></div>
+                            <div className="song-text">
+                              <p className="song-title">Heat Waves</p>
+                              <p className="song-artist">Glass Animals</p>
+                              <p className="song-genre">Indie Pop</p>
+                            </div>
+                          </div>
+                          <div className="song-info-box">
+                            <div className="circle"></div>
+                            <div className="song-text">
+                              <p className="song-title">Heat Waves</p>
+                              <p className="song-artist">Glass Animals</p>
+                              <p className="song-genre">Indie Pop</p>
+                            </div>
+                          </div>
+                          <div className="song-info-box">
+                            <div className="circle"></div>
+                            <div className="song-text">
+                              <p className="song-title">Levitating</p>
+                              <p className="song-artist">Dua Lipa</p>
+                              <p className="song-genre">Dance</p>
+                            </div>
+                          </div>
+                          <div className="song-info-box">
+                            <div className="circle"></div>
+                            <div className="song-text">
+                              <p className="song-title">Levitating</p>
+                              <p className="song-artist">Dua Lipa</p>
+                              <p className="song-genre">Dance</p>
+                            </div>
+                          </div>
+                          <div className="song-info-box">
+                            <div className="circle"></div>
+                            <div className="song-text">
+                              <p className="song-title">Levitating</p>
+                              <p className="song-artist">Dua Lipa</p>
+                              <p className="song-genre">Dance</p>
+                            </div>
+                          </div>
+                          <div className="song-info-box">
+                            <div className="circle"></div>
+                            <div className="song-text">
+                              <p className="song-title">Levitating</p>
+                              <p className="song-artist">Dua Lipa</p>
+                              <p className="song-genre">Dance</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </section>
+
+        </div>
+        <div className="scroll-section">
+            <section className="home-featured">
+                      <h2>Recent Statistics</h2>
+                      <p className="subtext">Your Featured Trends</p>
 
                       <div className="grid-wrapper">
                         <div className="responsive-grid">
