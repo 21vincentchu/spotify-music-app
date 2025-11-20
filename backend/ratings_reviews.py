06ecd39a5b6df4bc5d04f4db6a61d2031c939768
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-11-19 20:32:07 -0600

    merge

--- backend/ratings_reviews.py
+++ backend/ratings_reviews.py
@@ -1,2 +1,2 @@
-## database calls go here 
-##get + create album review, album rating, song review, song rating 
+## Flask App[ ] 
+##get, edit, delete, create album review, album rating, song review, song rating 
