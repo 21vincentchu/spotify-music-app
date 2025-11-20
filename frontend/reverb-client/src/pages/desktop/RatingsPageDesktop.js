f0edd4c9843af957d471ea505c21c2a7760a7e68
amandanngo <amandanngo@gmail.com>
2025-11-20 14:43:40 -0600

    Ratings Side Tabs

--- frontend/reverb-client/src/pages/desktop/RatingsPageDesktop.js
+++ frontend/reverb-client/src/pages/desktop/RatingsPageDesktop.js
@@ -1,9 +1,32 @@
 import Footer from "../../components/shared/Footer";
+import { useState } from "react";
 
 function RatingsPage() {
+
+  const [myFeaturedTab, setMyFeaturedTab] = useState('songs');
+
   return (
-    <div className="page">
-        <p>Site under Construction</p>
+    <div className="recommendations-page">
+          <div className="type-tabs">
+            <button className="type-button">Ratings</button>
+            <button className="type-button">Reviews</button>
+          </div>
+        <div className="ratings-container round-outline blue-box-shadow">
+          <div className="featured-tabs">
+              <button
+                className={myFeaturedTab === 'songs' ? 'active' : ''}
+                onClick={() => setMyFeaturedTab('songs')}
+              >
+                Songs
+              </button>
+              <button
+                className={myFeaturedTab === 'albums' ? 'active' : ''}
+                onClick={() => setMyFeaturedTab('albums')}
+              >
+                Albums
+              </button>
+            </div>
+        </div>
         <Footer />
     </div>
   )
