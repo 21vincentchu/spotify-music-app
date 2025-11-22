e6b8f6defd9f48267dec7c281f095cbb034b06bf
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-11-21 19:46:15 -0600

    modify album route

--- backend/ratings_route.py
+++ backend/ratings_route.py
@@ -157,3 +157,20 @@ def update_song_rating_route():
         return jsonify({"error": "Rating not found or no changes made"}), 404
     
     return jsonify({"message": "Song rating updated successfully"}), 200
+
+@ratings_bp.route("/rating/album", methods=["PATCH"])
+def update_album_rating_route():
+    userName = request.args.get("userName")
+    spotifyAlbumId = request.args.get("spotifyAlbumId")
+    rating = request.args.get("rating")
+    comment = request.args.get("comment")
+
+    if not userName or not spotifyAlbumId:
+        return jsonify({"error": "Missing required fields"}), 400
+    
+    updated = update_album_rating(userName=userName, spotifyAlbumId=spotifyAlbumId, rating=rating, comment=comment)
+
+    if not updated:
+        return jsonify({"error": "Rating not found or no changes made"}), 404
+    
+    return jsonify({"message": "Song rating updated successfully"}), 200
