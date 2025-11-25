d789c956f57852e467a758499dca7dcf1d617203
amandanngo <amandanngo@gmail.com>
2025-11-25 01:10:13 -0600

    Delete Song Rating

--- frontend/reverb-client/src/pages/desktop/RatingDetailPageDesktop.js
+++ frontend/reverb-client/src/pages/desktop/RatingDetailPageDesktop.js
@@ -3,9 +3,7 @@ import { Rating } from '@mui/material';
 import axios from 'axios';
 import config from '../../config';
 import { redirect, useParams } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-
-
+import { useNavigate } from "react-router-dom";
 
 function RatingsDetailPageDesktop() {
 
@@ -14,11 +12,13 @@ function RatingsDetailPageDesktop() {
     const [ratingData, setRatingData] = useState(null);
     const [rating, setRating] = useState(0);
     const [comment, setComment] = useState('');
-    
+    const navigate = useNavigate();
+
+
     useEffect(() => {   
         fetchData();
-        fetchRating();
-    }, [spotifyId]);
+        // fetchRating();
+    }, []);
  
     const fetchData = async () => {
         try {
@@ -26,28 +26,33 @@ function RatingsDetailPageDesktop() {
                 `${config.API_URL}/api/ratings/song/${spotifyId}`,
                 { withCredentials: true }
             );
-            console.log('Rating found:', response.data);
+            console.log('Song found:', response.data);
+            if(response.data.userRating){
+                setIsRated(true);
+            }
             setRatingData(response.data.song);
+            setRating(response.data.userRating.rating || 0);
+            setComment(response.data.userRating.comment || '');
         } catch (error) {
             console.error('Error fetching data:', error);
         }
         
     };
 
-    const fetchRating = async () => {
-        axios.get(`${config.API_URL}/api/ratings/rated-song/${spotifyId}`, {
-            withCredentials: true
-        })
-        .then(response => { 
-            console.log('Rating found:', response.data);
-            setRating(response.data.rating || 0);
-            setComment(response.data.comment || '');
-            setIsRated(true);
-        })
-        .catch (error => {
-            console.error('No rating found', error);
-        } )
-    }
+    // const fetchRating = async () => {
+    //     axios.get(`${config.API_URL}/api/ratings/rated-song/${spotifyId}`, {
+    //         withCredentials: true
+    //     })
+    //     .then(response => { 
+    //         console.log('Rating found:', response.data);
+    //         setRating(response.data.rating || 0);
+    //         setComment(response.data.comment || '');
+    //         setIsRated(true);
+    //     })
+    //     .catch (error => {
+    //         console.error('No rating found', error);
+    //     } )
+    // }
 
     const handleSave = async () => {
 
@@ -94,42 +99,59 @@ function RatingsDetailPageDesktop() {
         }
     };
 
-  return (
-    <div className='ratings-detail-page'>
-        <div className='ratings-detail-container round-outline blue-box-shadow'>
-            
-            <div className='ratings-detail-info'>
-                <img className="round-outline" src={ratingData?.imageUrl} alt={ratingData?.songName} />
-                <div>
+    const handleDelete = async () => { 
+        try {
+            const response = await axios.delete(
+                `${config.API_URL}/api/ratings/song/${spotifyId}`,
+                { withCredentials: true }
+            );
+            console.log('Song deleted:', response.data);
+            navigate('/ratings');
+
+        } catch (error) {
+            console.error('Error deleting data:', error);
+        }
+    }
+
+    return (
+        <div className='ratings-detail-page'>
+            <div className='ratings-detail-container round-outline blue-box-shadow'>
+                
+                <div className='ratings-detail-info'>
+                    <img className="round-outline" src={ratingData?.imageUrl} alt={ratingData?.songName} />
                     <div>
-                        <p className='song-name'>{ratingData?.songName}</p>
-                        <p className='artist-name'>{ratingData?.artistName}</p>  
-                    </div>  
-                    <div className='star-rating-comp'>       
-                        <Rating 
-                            name="half-rating" 
-                            value={rating} 
-                            precision={0.5}
-                            onChange={(event, newValue) => setRating(newValue)}
-                        />  
-                        <p>{rating}</p>  
+                        <div>
+                            <p className='song-name'>{ratingData?.songName}</p>
+                            <p className='artist-name'>{ratingData?.artistName}</p>  
+                        </div>  
+                        <div className='star-rating-comp'>       
+                            <Rating 
+                                name="half-rating" 
+                                value={rating} 
+                                precision={0.5}
+                                onChange={(event, newValue) => setRating(newValue)}
+                            />  
+                            <p>{rating}</p>  
+                        </div>
                     </div>
                 </div>
-            </div>
-            
-            <div className='ratings-detail-actions'>
-                <textarea 
-                    className="round-outline" 
-                    value={comment}
-                    onChange={(e) => setComment(e.target.value)}
-                />
-                <button type="submit" onClick={handleSave}>Save</button>
+                
+                <div className='ratings-detail-actions'>
+                    <textarea 
+                        className="round-outline" 
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                    />
+                    <div className=''>
+                        <button type="submit" onClick={handleDelete}>Delete</button>
+                        <button type="submit" onClick={handleSave}>Save</button>
+                    </div>
+                
+                </div>
             </div>
         </div>
-    </div>
 
-  )
-
-}
+        )
+    }
 
 export default RatingsDetailPageDesktop;
