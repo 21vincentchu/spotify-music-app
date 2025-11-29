410d39a030d251eeb86a69e847617614710d0c23
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-11-28 23:40:19 -0600

    searching built out by track id for now

--- backend/search/search.py
+++ backend/search/search.py
@@ -1,12 +1,14 @@
 from db import get_db
 from typing import List, Dict
-from ratings import ratings_route
+from ratings.ratings_route import *
+from auth import get_authenticated_spotify_client
+
 """
 search an artist, album, song in db 
 if this is empty, use the spotify API 
 Returns: List[Dict]
 """
-def search_music_all(searchQuery: str, limit: int = 20):
+def search_music_all(searchQuery: str, limit: int = 20) -> List[Dict]:
     conn = get_db()
     cursor = conn.cursor(dictionary=True)
     search_pattern = f"%{searchQuery}%"
@@ -38,7 +40,7 @@ def search_music_all(searchQuery: str, limit: int = 20):
                      UNION
                      SELECT albumName, artistName, spotifyAlbumId, imageUrl FROM FeaturedAlbum
                      ) AS albums
-                       WHERE albumName LIKE %s OR artistNAME LIKE %s
+                       WHERE albumName LIKE %s OR artistName LIKE %s
                        LIMIT %s;
                        """, (search_pattern, search_pattern, limit))
         results.extend(cursor.fetchall())
@@ -47,7 +49,7 @@ def search_music_all(searchQuery: str, limit: int = 20):
                 Select artistName as name, spotifyArtistId as spotifyId, imageUrl, 'artist' as type
                        
                 FROM(
-                       SELECT artistName, spotifyArtistId, imageUrl FROM topArtist
+                       SELECT artistName, spotifyArtistId, imageUrl FROM TopArtist
                        UNION
                        SELECT artistName, spotifyArtistId, imageUrl FROM FeaturedArtist
                        ) AS artists   
@@ -57,10 +59,48 @@ def search_music_all(searchQuery: str, limit: int = 20):
         results.extend(cursor.fetchall())
                        
         if not results:
-            ratings_route.fetch_song_from_spotify, ratings_route.fetch_album_from_spotify
-
+            sp = get_authenticated_spotify_client()
+            spotify_id = searchQuery.strip()
+            try:
+                 song = fetch_song_from_spotify(sp, spotify_id)
+                 results.append({
+                      'name': song['songName'],
+                      'artist': song['artistName'],
+                      'spotifyId': song['spotifyTrackId'],
+                      'imageUrl': song['imageUrl'],
+                      'type': 'song'
+                 })
+                 return results
+            except Exception as e:
+                 print(f"song search failed: {e}")
+            
+            try:
+                 album = fetch_album_from_spotify(sp, spotify_id)
+                 results.append({
+                      'name': album['albumName'],
+                      'artist': album['artistName'],
+                      'spotifyId': album['spotifyAlbumId'],
+                      'imageUrl': album['imageUrl'],
+                      'type': 'album'
+                        
+                 })
+                 return results
+            except Exception as e:
+                 print(f"album searched failed: {e}")
+            try:
+                 artist = fetch_artist_from_spotify(sp, spotify_id)
+                 results.append({
+                      'name': artist['artistName'],
+                      'spotifyId': artist['spotifyArtistId'],
+                      'imageUrl': artist['imageUrl'],
+                      'type': 'artist'
+                 })
+                 return results
+            except Exception as e:
+                 print(f"artist searched failed: {e}")
+                         
         return results
-    
+             
     finally:
             cursor.close()
             conn.close() 
